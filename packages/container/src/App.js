c8b0008f437267bda3be470d79c369070a48c632
LeeTrent <lee.trent.1@gmail.com>
2021-12-01 20:36:46 -0600

    Fixing deployment build errors

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -6,6 +6,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return <div>
         <h1>Container</h1>
+        <h3>Hello</h3>
         <hr />
         <MarketingApp />
     </div>
