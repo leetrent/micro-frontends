083ac87ad3b0820856ab4cb249ad61fc65229dfe
LeeTrent <lee.trent.1@gmail.com>
2021-12-03 11:17:16 -0600

    Updated App.js to trigger Github action workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -6,8 +6,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return <div>
         <h1>Container</h1>
-        <h3>Hello</h3>
-        <hr />
+        <h3>Automating AWS Cloudfront Invalidation</h3>
         <hr />
         <MarketingApp />
     </div>
