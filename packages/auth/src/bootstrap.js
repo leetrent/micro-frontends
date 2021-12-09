a9c48d62d69eb354f357347367d022cf5464b258
LeeTrent <lee.trent.1@gmail.com>
2021-12-08 21:08:02 -0600

    Got auth subproject running on localhost

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+///////////////////////////////////////////////
+// Mount function to start up this application:
+///////////////////////////////////////////////
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+    
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate( { pathname: nextPathname }) {
+            //console.log("[auth][bootstrap][mount][onParentNavigate] => (nextPathname):", nextPathname);
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
+};
+
+//////////////////////////////////////////////////////////
+// If we are in development mode and running in isolation,
+// call the mount function above immediately:
+//////////////////////////////////////////////////////////
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_auth-dev-root');
+    if (devRoot) {
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
+    }
+}
+
+// If we are running this application through a container,
+// export the mount function;
+export { mount };
