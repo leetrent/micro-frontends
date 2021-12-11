10c8a73210adecbcb8eb3ee42004668b7778567f
LeeTrent <lee.trent.1@gmail.com>
2021-12-10 20:33:33 -0600

    Added auth sub-module

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -11,6 +11,7 @@ const generateClassName = createGenerateClassName({
 
 export default ( {history} ) => {
     return (
+        
         <div>
             <StylesProvider generateClassName={generateClassName}>
                 <Router history={history}>
